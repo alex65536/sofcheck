0c991062619dfd05f4d5bc9046c67669f0183c3f
Alexander Kernozhitsky <sh200105@mail.ru>
2020-07-12 02:02:41 +0300

    Reformat magic.h

--- src/core/private/magic.h
+++ src/core/private/magic.h
@@ -1,19 +1,19 @@
 #ifndef MAGIC_H_INCLUDED
 #define MAGIC_H_INCLUDED
 
-#include "core/types.h"
+#include <immintrin.h>  // Use _pext_u64() from BMI2 instruction set
 
-#include <immintrin.h> // Use _pext_u64() from BMI2 instruction set
+#include "core/types.h"
 
 namespace SoFCore {
 namespace Private {
 
 struct MagicEntry {
   const bitboard_t *arr;
-  bitboard_t msk; // Mask for _pext_u64()
-  bitboard_t postMsk; // Mask to distinguish between shared attacks
+  bitboard_t msk;      // Mask for _pext_u64()
+  bitboard_t postMsk;  // Mask to distinguish between shared attacks
 };
- 
+
 extern MagicEntry g_magicRook[64];
 extern MagicEntry g_magicBishop[64];
 
@@ -26,10 +26,10 @@ inline bitboard_t rookAttackBitboard(bitboard_t occupied, cell_t pos) {
 
 inline bitboard_t bishopAttackBitboard(bitboard_t occupied, cell_t pos) {
   const MagicEntry &entry = g_magicBishop[pos];
-  return entry.arr[_pext_u64(occupied, entry.msk)] & entry.postMsk;  
+  return entry.arr[_pext_u64(occupied, entry.msk)] & entry.postMsk;
 }
 
-}
-}
+}  // namespace Private
+}  // namespace SoFCore
 
-#endif // MAGIC_H_INCLUDED
+#endif  // MAGIC_H_INCLUDED
