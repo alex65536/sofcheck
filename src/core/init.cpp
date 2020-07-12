61db555bac6822804618ec0c7bd09a8bac5934a1
Alexander Kernozhitsky <sh200105@mail.ru>
2020-07-12 13:33:09 +0300

    Protect init() from repetitive iniitalization

--- src/core/init.cpp
+++ src/core/init.cpp
@@ -1,10 +1,17 @@
 #include "init.h"
 
+#include <atomic>
+
 #include "core/private/magic.h"
 
 namespace SoFCore {
 
+std::atomic_flag initialized;
+
 void init() {
+  if (initialized.test_and_set()) {
+    return;
+  }
   Private::initMagic();
 }
 
