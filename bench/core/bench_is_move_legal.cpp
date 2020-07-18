bf24d92d4a6bcece9407feccc61128a4bcd3a122
Alexander Kernozhitsky <sh200105@mail.ru>
2020-07-19 00:39:27 +0300

    Add benchmark for isMoveLegal

---
+++ bench/core/bench_is_move_legal.cpp
@@ -0,0 +1,31 @@
+#include <benchmark/benchmark.h>
+
+#include "core/bench_boards.h"
+#include "core/board.h"
+#include "core/init.h"
+#include "core/movegen.h"
+
+inline void runCheckValid(benchmark::State &state, const char *fen) {
+  using namespace SoFCore;
+
+  init();
+
+  Board board = Board::fromFen(fen).unwrap();
+  Move moves[240];
+  size_t cnt = genAllMoves(board, moves);
+
+  for (auto _ : state) {
+    (void)_;
+    for (size_t i = 0; i < cnt; ++i) {
+      benchmark::DoNotOptimize(isMoveValid(board, moves[i]));
+    }
+  }
+}
+
+#define BENCH_DO(name)                                                                            \
+  static void BM_CheckValid##name(benchmark::State &state) { runCheckValid(state, g_fen##name); } \
+  BENCHMARK(BM_CheckValid##name);
+#include "core/bench_xmacro.h"
+#undef BENCH_DO
+
+BENCHMARK_MAIN();
