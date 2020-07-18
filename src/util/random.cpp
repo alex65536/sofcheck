b7b5c5f73dc33301ba2e95efae2d0e07c7427a8d
Alexander Kernozhitsky <sh200105@mail.ru>
2020-07-19 00:29:18 +0300

    Make clang-tidy happy

--- src/util/random.cpp
+++ src/util/random.cpp
@@ -3,9 +3,19 @@
 #include <chrono>
 #include <random>
 
+#include "util/misc.h"
+
 namespace SoFUtil {
 
-static std::mt19937_64 g_randGen(std::chrono::steady_clock::now().time_since_epoch().count());
+// NOLINTNEXTLINE(cert-err58-cpp): possible exception is caught, but clang-tidy doesn't understand
+static std::mt19937_64 g_randGen = []() noexcept {
+  // Fix cert-err58-cpp, handle the possible exception during static initialization
+  try {
+    return std::mt19937_64(std::chrono::steady_clock::now().time_since_epoch().count());
+  } catch (...) {
+    panic("Error initializing g_randGen");
+  }
+}();
 
 uint64_t random() { return g_randGen(); }
 
