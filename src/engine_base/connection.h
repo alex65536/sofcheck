a12070b9e74b997ede8ec1bac7c7fdebee0f68d4
Alexander Kernozhitsky <sh200105@mail.ru>
2020-07-20 23:32:53 +0300

    Split connection.h into two headers

--- src/engine_base/connection.h
+++ src/engine_base/connection.h
@@ -5,35 +5,11 @@
 
 #include "engine_base/api_base.h"
 #include "engine_base/client.h"
+#include "engine_base/connector.h"
 #include "engine_base/server.h"
 #include "util/result.h"
 
 namespace SoFEngineBase {
-
-// Results of `poll()` calls
-enum class PollResult {
-  // Success
-  Ok,
-  // Success, but received shutdown message
-  Shutdown,
-  // I/O error
-  IOError,
-  // Unknown error
-  RuntimeError
-};
-
-// Abstract class that is used to connect to client or server. It can wait until it gets some data
-class Connector {
-public:
-  // Block current thread until the connector gets some data
-  virtual PollResult poll() = 0;
-};
-
-// Connector that emulates a client. Calls server API
-class ClientConnector : public Client, public Connector {};
-
-// Connector that emulates a server. Calls server API
-class ServerConnector : public Server, public Connector {};
 
 // Class that holds the connection between the client and the server
 class Connection {
---
+++ src/engine_base/connector.h
@@ -0,0 +1,36 @@
+#ifndef SOF_ENGINE_BASE_CONNECTOR_INCLUDED
+#define SOF_ENGINE_BASE_CONNECTOR_INCLUDED
+
+#include "engine_base/client.h"
+#include "engine_base/server.h"
+
+namespace SoFEngineBase {
+
+// Results of `poll()` calls
+enum class PollResult {
+  // Success
+  Ok,
+  // Success, but received shutdown message
+  Shutdown,
+  // I/O error
+  IOError,
+  // Unknown error
+  RuntimeError
+};
+
+// Abstract class that is used to connect to client or server. It can wait until it gets some data
+class Connector {
+public:
+  // Block current thread until the connector gets some data
+  virtual PollResult poll() = 0;
+};
+
+// Connector that emulates a client. Calls server API
+class ClientConnector : public Client, public Connector {};
+
+// Connector that emulates a server. Calls server API
+class ServerConnector : public Server, public Connector {};
+
+}  // namespace SoFEngineBase
+
+#endif  // SOF_ENGINE_BASE_CONNECTOR_INCLUDED
