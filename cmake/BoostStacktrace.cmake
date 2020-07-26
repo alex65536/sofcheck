7ce1ef7065c388c51a37aef545d42d5506b59e70
Alexander Kernozhitsky <sh200105@mail.ru>
2020-07-26 14:32:36 +0300

    Use boost::stacktrace in panic as optional dependency

---
+++ cmake/BoostStacktrace.cmake
@@ -0,0 +1,21 @@
+# Detects `boost::stacktrace`. If it's present, then the following variables are set:
+# - `USE_BOOST_STACKTRACE`
+# - `BOOST_STACKTRACE_TARGET` is set to name of the detected `boost::stacktrace` backend
+
+set(BOOST_STACKTRACE_BACKENDS
+  stacktrace_windbg_cached
+  stacktrace_windbg
+  stacktrace_backtrace
+  stacktrace_addr2line
+  stacktrace_basic
+)
+find_package(Boost 1.65.0 OPTIONAL_COMPONENTS ${BOOST_STACKTRACE_BACKENDS})
+if(Boost_FOUND)
+  foreach(backend ${BOOST_STACKTRACE_BACKENDS})
+    if(boost_${backend}_FOUND)
+      set(USE_BOOST_STACKTRACE ON)
+      set(BOOST_STACKTRACE_TARGET Boost::${backend})
+      break()
+    endif()
+  endforeach()
+endif()
--- src/util/misc.cpp
+++ src/util/misc.cpp
@@ -3,10 +3,19 @@
 #include <exception>
 #include <iostream>
 
+#include "config.h"
+
+#ifdef USE_BOOST_STACKTRACE
+#include <boost/stacktrace/stacktrace.hpp>
+#endif
+
 namespace SoFUtil {
 
 [[noreturn]] void panic(const char *message) {
   std::cerr << "Program panicked: " << message << std::endl;
+#ifdef USE_BOOST_STACKTRACE
+  std::cerr << "Stack trace: " << std::endl << boost::stacktrace::stacktrace() << std::flush;
+#endif
   std::terminate();
 }
 
