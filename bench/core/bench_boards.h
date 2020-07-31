0c49f37cfda89f044c71702a605884c05838271b
Alexander Kernozhitsky <sh200105@mail.ru>
2020-07-31 23:02:09 +0300

    Add new position to benchmark on

--- bench/core/bench_boards.h
+++ bench/core/bench_boards.h
@@ -8,6 +8,7 @@ const char *g_fenOpenPosition = "4r1k1/3R1ppp/8/5P2/p7/6PP/4pK2/1rN1B3 w - - 4 4
 const char *g_fenQueens = "6K1/8/8/1k3q2/3Q4/8/8/8 w - - 0 1";
 const char *g_fenPawnsMove = "4k3/pppppppp/8/8/8/8/PPPPPPPP/4K3 w - - 0 1";
 const char *g_fenPawnsAttack = "4k3/8/8/pppppppp/PPPPPPPP/8/8/4K3 w - - 0 1";
+const char *g_fenPawnsPromote = "8/PPPPPPPP/8/2k1K3/8/8/pppppppp/8 w - - 0 1";
 const char *g_fenCydonia = "5K2/1N1N1N2/8/1N1N1N2/1n1n1n2/8/1n1n1n2/5k2 w - - 0 1";
 const char *g_fenMax = "3Q4/1Q4Q1/4Q3/2Q4R/Q4Q2/3Q4/NR4Q1/kN1BB1K1 w - - 0 1";
 
