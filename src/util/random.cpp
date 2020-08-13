39c5bc8c9979c376a198b87a2136e95f6a3987a8
Alexander Kernozhitsky <sh200105@mail.ru>
2020-08-14 00:52:53 +0300

    Make random thread-safe

--- src/util/random.cpp
+++ src/util/random.cpp
@@ -2,26 +2,27 @@
 
 #include <chrono>
 #include <random>
+#include <thread>
 
 #include "config.h"
 #include "util/misc.h"
 
 namespace SoFUtil {
 
-// NOLINTNEXTLINE(cert-err58-cpp): possible exception is caught, but clang-tidy doesn't understand
-static std::mt19937_64 g_randGen = []() noexcept {
-// Fix cert-err58-cpp, handle the possible exception during static initialization
-#ifndef USE_NO_EXCEPTIONS
-  try {
-#endif
-    return std::mt19937_64(std::chrono::steady_clock::now().time_since_epoch().count());
-#ifndef USE_NO_EXCEPTIONS
-  } catch (...) {
-    panic("Error initializing g_randGen");
-  }
-#endif
-}();
+static uint64_t makeRandomSeed() {
+  return std::chrono::steady_clock::now().time_since_epoch().count() +
+         static_cast<uint64_t>(std::hash<std::thread::id>()(std::this_thread::get_id()));
+}
 
-uint64_t random() { return g_randGen(); }
+struct RandGenWrapper {
+  std::mt19937_64 gen;
+
+  RandGenWrapper() noexcept : gen(makeRandomSeed()) {}
+};
+
+uint64_t random() {
+  static thread_local RandGenWrapper wrapper;
+  return wrapper.gen();
+}
 
 }  // namespace SoFUtil
