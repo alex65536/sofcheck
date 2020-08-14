66a7efa25869a8b06a73df407cd474eef739939b
Alexander Kernozhitsky <sh200105@mail.ru>
2020-08-15 00:04:06 +0300

    Make libbenchmark optional dependency

--- cmake/Benchmark.cmake
+++ cmake/Benchmark.cmake
@@ -1,6 +1,10 @@
-find_package(benchmark REQUIRED)
+find_package(benchmark)
 
 function(target_benchmark target)
+  if(NOT benchmark_FOUND)
+    message(SEND_ERROR "Cannot create a benchmark target, as Google Benchmark is not found!")
+    return()
+  endif()
   target_include_directories(${target} PRIVATE ${PROJECT_SOURCE_DIR}/bench)
   target_link_libraries(${target} benchmark::benchmark)
   if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
