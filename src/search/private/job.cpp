fc2679d24be1794714c3e844a84f2ecfe8946855
Alexander Kernozhitsky <sh200105@mail.ru>
2020-08-17 01:38:51 +0300

    Refresh the hash entry on hit

--- src/search/private/job.cpp
+++ src/search/private/job.cpp
@@ -235,6 +235,8 @@ score_t Searcher::doSearch(const size_t depth, const size_t idepth, score_t alph
       switch (data.bound()) {
         case PositionCostBound::Exact: {
           frame.bestMove = hashMove;
+          // Refresh the hash entry, as it may come from older epoch
+          tt_.store(board_.hash, data);
           return score;
         }
         case PositionCostBound::Lowerbound: {
