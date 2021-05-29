a65ed799f921fc39a16b6c1de49b2c736045dda9
Alexander Kernozhitsky <sh200105@mail.ru>
2021-05-30 01:14:37 +0300

    Check that the file is really opened

--- gen/gen_main.cpp
+++ gen/gen_main.cpp
@@ -11,9 +11,13 @@ int main(int argc, char **argv) {
   }
   if (argc == 2 && strcmp(argv[1], "-h") != 0 && strcmp(argv[1], "--help") != 0) {
     std::ofstream file(argv[1]);
+    if (!file.is_open()) {
+      std::cerr << "Unable to open " << argv[1] << std::endl;
+      return 1;
+    }
     doGenerate(file);
     return 0;
   }
-  std::cerr << "usage: " << argv[0] << " OUT_FILE" << std::endl;
+  std::cerr << "Usage: " << argv[0] << " OUT_FILE" << std::endl;
   return 1;
 }
