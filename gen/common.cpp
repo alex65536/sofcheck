4a80a45dbc26b1b69bc01a7508d051e2e56de51c
Alexander Kernozhitsky <sh200105@mail.ru>
2021-05-30 12:53:15 +0300

    Make generator output nicer

--- gen/common.cpp
+++ gen/common.cpp
@@ -7,12 +7,23 @@ void printBitboard(std::ostream &out, SoFCore::bitboard_t val) {
       << std::setfill(' ');
 }
 
+static size_t log10(size_t val) {
+  size_t res = 0;
+  size_t pow = 1;
+  while (pow <= val) {
+    pow *= 10;
+    ++res;
+  }
+  return res - 1;
+}
+
 template <typename T, typename ItemPrinter>
 static void printArrayCommon(std::ostream &out, const std::vector<T> &array, size_t indent,
                              ItemPrinter printer) {
   out << "[" << std::dec << array.size() << "] = {\n";
+  const size_t idxLen = (array.size() <= 1) ? 1 : log10(array.size() - 1) + 1;
   for (size_t i = 0; i < array.size(); ++i) {
-    out << std::string(indent + 4, ' ') << "/*" << std::setw(2) << i << "*/ ";
+    out << std::string(indent + 4, ' ') << "/*" << std::setw(idxLen) << i << "*/ ";
     printer(out, array[i]);
     if (i + 1 != array.size()) {
       out << ",";
