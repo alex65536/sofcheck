fbcb717806ecad44df933384024570919dfe6567
Alexander Kernozhitsky <sh200105@mail.ru>
2021-05-31 03:12:34 +0300

    Minor fixes

--- gen/common.cpp
+++ gen/common.cpp
@@ -6,13 +6,13 @@
 
 using SoFUtil::log10;
 
-void printBitboard(std::ostream &out, SoFCore::bitboard_t val) {
+void printBitboard(std::ostream &out, const SoFCore::bitboard_t val) {
   out << "0x" << std::hex << std::setw(16) << std::setfill('0') << val << std::dec
       << std::setfill(' ');
 }
 
 template <typename T, typename ItemPrinter>
-static void printArrayCommon(std::ostream &out, const std::vector<T> &array, size_t indent,
+static void printArrayCommon(std::ostream &out, const std::vector<T> &array, const size_t indent,
                              ItemPrinter printer) {
   out << "[" << std::dec << array.size() << "] = {\n";
   const size_t idxLen = (array.size() <= 1) ? 1 : log10(array.size() - 1) + 1;
@@ -28,21 +28,21 @@ static void printArrayCommon(std::ostream &out, const std::vector<T> &array, siz
 }
 
 void printCoordArray(std::ostream &out, const std::vector<SoFCore::coord_t> &array,
-                     const char *name, size_t indent) {
+                     const char *name, const size_t indent) {
   out << "constexpr coord_t " << name;
   printArrayCommon(out, array, indent,
                    [](std::ostream &out, const SoFCore::coord_t x) { out << static_cast<int>(x); });
 }
 
 void printBitboardArray(std::ostream &out, const std::vector<SoFCore::bitboard_t> &array,
-                        const char *name, size_t indent) {
+                        const char *name, const size_t indent) {
   out << "constexpr bitboard_t " << name;
   printArrayCommon(out, array, indent,
                    [](std::ostream &out, const SoFCore::bitboard_t bb) { printBitboard(out, bb); });
 }
 
 void printIntArray(std::ostream &out, const std::vector<int32_t> &array, const char *name,
-                   const char *typeName, size_t indent) {
+                   const char *typeName, const size_t indent) {
   out << "constexpr " << typeName << " " << name;
   printArrayCommon(out, array, indent, [](std::ostream &out, const int32_t x) { out << x; });
 }
