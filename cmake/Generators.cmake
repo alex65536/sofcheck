05b33daa51066af48f49f95a5c3aaad27c05beb4
Alexander Kernozhitsky <sh200105@mail.ru>
2021-06-01 01:42:31 +0300

    More simplfication

--- cmake/Generators.cmake
+++ cmake/Generators.cmake
@@ -5,6 +5,7 @@ include_guard(GLOBAL)
 add_library(sof_gen_common STATIC
   gen/common.cpp
 )
+target_include_directories(sof_gen_common PUBLIC gen/)
 target_link_libraries(sof_gen_common PUBLIC sof_util)
 
 add_library(sof_gen_main STATIC
@@ -18,16 +19,23 @@ add_library(sof_gen_json_main STATIC
 target_link_libraries(sof_gen_json_main PUBLIC sof_gen_common jsoncpp_lib)
 
 
-
 # Creates the directory in which `file` will be located
 function(_ensure_dir file)
   string(REGEX REPLACE "^(.*)/[^/]+$" "\\1" dir "${file}")
   file(MAKE_DIRECTORY "${dir}")
 endfunction()
 
-# Generates a file with generator target `gen_target`, which is compiled from source `gen_src`.
-# The result is stored in `${PROJECT_BINARY_DIR}/out_file`
-function(generate_file gen_target gen_src out_file)
+# Deduces target name from generator source file name `src` and stores the result in `result`
+function(_gen_get_name src result)
+  string(REPLACE "/" "_" src "${src}")
+  string(REGEX REPLACE "\.cpp$" "" src "${src}")
+  set("${result}" "${src}" PARENT_SCOPE)
+endfunction()
+
+# Generates a file with generator, which is compiled from source `gen_src`. The result is stored
+# in `${PROJECT_BINARY_DIR}/out_file`
+function(generate_file gen_src out_file)
+  _gen_get_name("${gen_src}" gen_target)
   add_executable("${gen_target}" "${gen_src}")
   target_link_libraries("${gen_target}" sof_gen_main)
   _ensure_dir("${PROJECT_BINARY_DIR}/${out_file}")
@@ -38,10 +46,11 @@ function(generate_file gen_target gen_src out_file)
   )
 endfunction()
 
-# Generates a file with generator target `gen_target`, which is compiled from source `gen_src`.
-# The generator reads the input from JSON file `${PROJECT_SOURCE_DIR}/in_file` and stores the
-# result in `${PROJECT_BINARY_DIR}/out_file`
-function(generate_file_json gen_target gen_src out_file in_file)
+# Generates a file with generator, which is compiled from source `gen_src`. The generator reads
+# the input from JSON file `${PROJECT_SOURCE_DIR}/in_file` and stores the result in
+# `${PROJECT_BINARY_DIR}/out_file`
+function(generate_file_json gen_src out_file in_file)
+  _gen_get_name("${gen_src}" gen_target)
   add_executable("${gen_target}" "${gen_src}")
   target_link_libraries("${gen_target}" sof_gen_json_main)
   _ensure_dir("${PROJECT_BINARY_DIR}/${out_file}")
