211e9c449ce3d188244ee7d9c3a6cf7dadff0026
Alexander Kernozhitsky <sh200105@mail.ru>
2021-12-27 01:06:40 +0300

    Add new optparse

---
+++ src/util/optparse.cpp
@@ -0,0 +1,61 @@
+#include "util/optparse.h"
+
+#include <cstdlib>
+#include <iostream>
+
+#include "util/strutil.h"
+
+namespace SoFUtil {
+
+static std::string getProgramName(const std::string &argv0) {
+  for (size_t pos = argv0.size() - 1; pos != static_cast<size_t>(-1); --pos) {
+    if (argv0[pos] == '/' || argv0[pos] == '\\') {
+      return argv0.substr(pos + 1);
+    }
+  }
+  return argv0;
+}
+
+OptParser::OptParser(int argc, const char *const *argv, const std::string &description)
+    : inner_(getProgramName(argv[0])), description_(description), argc_(argc), argv_(argv) {
+  inner_.set_width(width_);
+  inner_.add_options()("h,help", "Show help message");
+}
+
+void OptParser::setLongDescription(const std::string &longDescription) {
+  longDescription_ = longDescription;
+}
+
+cxxopts::OptionAdder OptParser::addOptions(const std::string group) {
+  return inner_.add_options(group);
+}
+
+void OptParser::setWidth(size_t width) {
+  width_ = width;
+  inner_.set_width(width_);
+}
+
+std::string OptParser::help() const {
+  std::string result;
+  for (auto ln : wordWrap(description_, width_)) {
+    result += ln;
+    result += "\n";
+  }
+  result += inner_.help() + "\n\n";
+  for (auto ln : wordWrap(longDescription_, width_)) {
+    result += ln;
+    result += "\n";
+  }
+  return result;
+}
+
+cxxopts::ParseResult OptParser::parse() {
+  auto options = inner_.parse(argc_, argv_);
+  if (options.count("help")) {
+    std::cout << help() << std::endl;
+    std::exit(0);
+  }
+  return options;
+}
+
+}  // namespace SoFUtil
---
+++ src/util/optparse.h
@@ -0,0 +1,47 @@
+#ifndef SOF_UTIL_OPTPARSE_INCLUDED
+#define SOF_UTIL_OPTPARSE_INCLUDED
+
+#include <cxxopts.hpp>
+#include <string>
+
+#include "util/result.h"
+
+namespace SoFUtil {
+
+// Wrapper for `cxxopts::Options`. It extends the features of `cxxopts`. For example, it allows to
+// use long descriptions with word-wrapping in help messages
+class OptParser {
+public:
+  OptParser(int argc, const char *const *argv, const std::string &description);
+
+  // Sets the long description. It is printed in the end of the help message
+  void setLongDescription(const std::string &longDescription);
+
+  // Adds options to the option parser. Works in the same way as `cxxopts::Options`
+  cxxopts::OptionAdder addOptions(const std::string group = "");
+
+  // Sets the output width
+  void setWidth(size_t width);
+
+  // Returns the help message as string
+  std::string help() const;
+
+  // Parses the command line arguments. When the parser is ready to pass the arguments to the main
+  // program, returns the parsed arguments. Otherwise, exits from the program
+  //
+  // Exiting from the program doesn't always mean that the arguments are invalid. For examples, if
+  // `-h` flag is specified, the parser just shows help and exits
+  cxxopts::ParseResult parse();
+
+private:
+  cxxopts::Options inner_;
+  std::string description_;
+  std::string longDescription_ = "";
+  int argc_;
+  const char *const *argv_;
+  size_t width_ = 78;
+};
+
+}  // namespace SoFUtil
+
+#endif  // SOF_UTIL_OPTPARSE_INCLUDED
